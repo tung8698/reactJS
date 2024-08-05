5ac5ef28362317393d9c6189302919816c2b7897
tung8698@ <tung8698@gmail.com>
2024-08-06 01:48:04 +0700

    hello

---
+++ hello/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container border my-3">
+        <div>
+          <img src="https://devmaster.edu.vn/images/logo.png"/>
+        </div>
+        <h1>Tôi bắt đầu học ReactJS</h1>
+    </div>
+  );
+}
+
+export default App;
